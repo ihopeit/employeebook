7644bf1c8c9f10a403579baad6f3af596b05e261
david euler <davidullua@aliyun.com>
2021-04-22 19:18:46 +0800

    列表页优化展示 & k8s deployment

--- book/admin.py
+++ book/admin.py
@@ -4,4 +4,14 @@ from django.contrib import admin
 
 from book.models import Employee
 
+class EmployeeAdmin(admin.ModelAdmin):
+    ## 编辑页不展示字段列表
+    exclude = ('created_date','updated_date')
+
+    list_display = ('empno', 'name', 'email', 'mobile', 'status', 'dept1', 'created_date', 'updated_date')
+
+    list_filter = ('dept1','status','office_city','edu_school')
+
+    search_fields = ('empno','name','email','mobile', 'dept1',)
+
 admin.site.register(Employee)
